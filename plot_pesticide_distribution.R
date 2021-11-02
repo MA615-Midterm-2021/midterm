4e0d420fe65ba282bc91a51aa8516e637210ecd8
jyli <jinyuli@bu.edu>
2021-11-02 12:29:11 -0400

    fix the plot_pie part: change the font size and the labels.

--- plot_pesticide_distribution.R
+++ plot_pesticide_distribution.R
@@ -3,9 +3,6 @@ source('test1025.R')
 if(!require(tidyverse)) install.packages("tidyverse")
 if(!require(ggplot2)) install.packages("ggplot2", repos = "http://cran.us.r-project.org")
 if(!require(ggpubr)) install.packages("ggpubr", repos = "http://cran.us.r-project.org")
-library(tidyverse)
-library(ggplot2)
-library(ggpubr)
 
 # load the data from test1025.R
 
@@ -21,6 +18,51 @@ df1 <- combine()
 #                                       stat = "identity", alpha = 0.5) +
 #   theme_bw() + labs(x = "State", y = "Pesticide")
 
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+  library(grid)
+  # Multiple plot function
+  #
+  # ggplot objects can be passed in ..., or to plotlist (as a list of ggplot objects)
+  # - cols:   Number of columns in layout
+  # - layout: A matrix specifying the layout. If present, 'cols' is ignored.
+  #
+  # If the layout is something like matrix(c(1,2,3,3), nrow=2, byrow=TRUE),
+  # then plot 1 will go in the upper left, 2 will go in the upper right, and
+  # 3 will go all the way across the bottom.
+  #
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+  
+  numPlots = length(plots)
+  
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                     ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+  
+  if (numPlots==1) {
+    print(plots[[1]])
+    
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+    
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+      
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
+}
+
 plot_chem <- function(year, dname1, plot_type, state = "California"){
   df1_test <- df1 %>% filter(dname == dname1 & Year == year)
   df1_stat <- df1_test %>% group_by(State, type) %>% summarise(count = n())
@@ -34,7 +76,8 @@ plot_chem <- function(year, dname1, plot_type, state = "California"){
     state %<>% toupper()
     df1_stat_state <- df1_stat %>% filter(State == state)
     percentage <- scales::percent(df1_stat_state$count / sum(df1_stat_state$count))
-    labs <- paste(df1_stat_state$type, '(', percentage, ')', sep = '') #set the labs
+    labs <- percentage #set the labs
+    # labs <- paste(df1_stat_state$type, '(', percentage, ')', sep = '') #set the labs
     # p <- df1_stat_state %>% ggplot(aes(x = "", y = count, fill = type)) + 
     #   geom_bar(stat = "identity", width = 1) +
     #   # geom_text(aes(label = labs)) +
@@ -46,7 +89,7 @@ plot_chem <- function(year, dname1, plot_type, state = "California"){
             fill = "type", 
             palette = "jco",
             label = labs, lab.pos = "in", lab.font = c(4, "white"))
-    p <- p1 %>% ggpar(title = paste0("Chemical usage percentage in ",state),
+    p <- p1 %>% ggpar(title = paste0("Chemical usage percentage in ",state, " in", year),
                       legend = "right")
   } else {
     stop("Please check the State!!")
@@ -55,8 +98,12 @@ plot_chem <- function(year, dname1, plot_type, state = "California"){
   p
 }
 # <demo>
+p1 <- plot_chem(2019, 'CHEMICAL', "pie")
+p2 <- plot_chem(2018, 'CHEMICAL', "pie")
+p3 <- plot_chem(2016, 'CHEMICAL', "pie")
 
-# plot_chem(2016, 'CHEMICAL', "bar")
+multiplot(p1,p2,p3, cols = 1)
+
 
 # </demo>
 
