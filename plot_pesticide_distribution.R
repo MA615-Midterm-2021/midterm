87efadcc3b13248e9e8ff3a7f0712e357bab0013
jyli <jinyuli@bu.edu>
2021-11-03 23:45:09 -0400

    updated the plot_pesticide_distribution.R by adding some comments.

--- plot_pesticide_distribution.R
+++ plot_pesticide_distribution.R
@@ -1,10 +1,11 @@
+# load the data from test1025.R
 source('test1025.R')
 
 if(!require(tidyverse)) install.packages("tidyverse")
 if(!require(ggplot2)) install.packages("ggplot2", repos = "http://cran.us.r-project.org")
 if(!require(ggpubr)) install.packages("ggpubr", repos = "http://cran.us.r-project.org")
 
-# load the data from test1025.R
+
 
 df1 <- combine()
 # dname1 <- 'CHEMICAL'
@@ -12,58 +13,20 @@ df1 <- combine()
 
 # df1_test <-  df1 %>% filter(dname == dname1 & Year == year)
 # df1_stat <- df1_test %>% group_by(State, type) %>% summarise(count = n())
+# df2_stat <- df1 %>% group_by(Year, State) %>% summarise(count = n())
+
 # # df1_stat_name <- df1_test %>% group_by(State, type, Pesticide_old)
 #   
 # p <- df1_stat %>% ggplot() + geom_bar(mapping = aes(x = as.factor(State), y = count, fill = type), 
 #                                       stat = "identity", alpha = 0.5) +
 #   theme_bw() + labs(x = "State", y = "Pesticide")
 
-multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
-  library(grid)
-  # Multiple plot function
-  #
-  # ggplot objects can be passed in ..., or to plotlist (as a list of ggplot objects)
-  # - cols:   Number of columns in layout
-  # - layout: A matrix specifying the layout. If present, 'cols' is ignored.
-  #
-  # If the layout is something like matrix(c(1,2,3,3), nrow=2, byrow=TRUE),
-  # then plot 1 will go in the upper left, 2 will go in the upper right, and
-  # 3 will go all the way across the bottom.
-  #
-  # Make a list from the ... arguments and plotlist
-  plots <- c(list(...), plotlist)
-  
-  numPlots = length(plots)
-  
-  # If layout is NULL, then use 'cols' to determine layout
-  if (is.null(layout)) {
-    # Make the panel
-    # ncol: Number of columns of plots
-    # nrow: Number of rows needed, calculated from # of cols
-    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
-                     ncol = cols, nrow = ceiling(numPlots/cols))
-  }
-  
-  if (numPlots==1) {
-    print(plots[[1]])
-    
-  } else {
-    # Set up the page
-    grid.newpage()
-    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
-    
-    # Make each plot, in the correct location
-    for (i in 1:numPlots) {
-      # Get the i,j matrix positions of the regions that contain this subplot
-      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
-      
-      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
-                                      layout.pos.col = matchidx$col))
-    }
-  }
-}
-
 plot_chem <- function(year, dname1, plot_type, state = "California"){
+  # plot the Chemical observations in different states and years
+  # parameters : year --- select a specific year we want to look into
+  #              dname1 --- "ORGANIC"/"CHEMICAL", now we only support "CHEMICAL"
+  #             plot_type --- "pie"/"bar", the type of figure we want to plot
+  #             state --- select a specific state we want to look into
   df1_test <- df1 %>% filter(dname == dname1 & Year == year)
   df1_stat <- df1_test %>% group_by(State, type) %>% summarise(count = n())
   if (plot_type == "bar"){
@@ -71,11 +34,12 @@ plot_chem <- function(year, dname1, plot_type, state = "California"){
       geom_bar(mapping = 
                  aes(x = as.factor(State), y = count, fill = type), stat = "identity", alpha = 0.5) +
       theme_bw() +
-      labs(x = "State", y = "Pesticide")
+      labs(x = "State", y = "Pesticide")  # reset the labs
   } else if (plot_type == "pie"){
     state %<>% toupper()
-    df1_stat_state <- df1_stat %>% filter(State == state)
-    percentage <- scales::percent(df1_stat_state$count / sum(df1_stat_state$count))
+    df1_stat_state <- df1_stat %>% filter(State == state) # select the specific state
+    # calculate the percentage of observation amount in one state among the total amount of observations 
+    percentage <- scales::percent(df1_stat_state$count / sum(df1_stat_state$count)) 
     labs <- percentage #set the labs
     # labs <- paste(df1_stat_state$type, '(', percentage, ')', sep = '') #set the labs
     # p <- df1_stat_state %>% ggplot(aes(x = "", y = count, fill = type)) + 
@@ -89,7 +53,7 @@ plot_chem <- function(year, dname1, plot_type, state = "California"){
             fill = "type", 
             palette = "jco",
             label = labs, lab.pos = "in", lab.font = c(4, "white"))
-    p <- p1 %>% ggpar(title = paste0("Chemical usage percentage in ",state, " in", year),
+    p <- p1 %>% ggpar(title = paste0("Chemical usage percentage in ",state, " in ",year), # add the legend
                       legend = "right")
   } else {
     stop("Please check the State!!")
@@ -98,33 +62,24 @@ plot_chem <- function(year, dname1, plot_type, state = "California"){
   p
 }
 # <demo>
-p1 <- plot_chem(2019, 'CHEMICAL', "pie")
-p2 <- plot_chem(2018, 'CHEMICAL', "pie")
-p3 <- plot_chem(2016, 'CHEMICAL', "pie")
-
-multiplot(p1,p2,p3, cols = 1)
-
-
+# p1 <- plot_chem(2019, 'CHEMICAL', "bar")
+# p2 <- plot_chem(2016, 'CHEMICAL', "bar")
+# p3 <- plot_chem(2019, 'CHEMICAL', "pie")
+# p4 <- plot_chem(2019, 'CHEMICAL', "pie", "Florida")
+# p5 <- plot_chem(2016, 'CHEMICAL', "pie")
+# p6 <- plot_chem(2016, 'CHEMICAL', "pie", "Florida")
+# 
+# p3
+# p4
+# p5
+# p6
 # </demo>
 
+# Below is the html code to create one scroll-box
+
+# <div style="width:150px;height:150px;line-height:3em;overflow:scroll;padding:5px;">
+#   This 'div' element uses 'overflow:scroll' to create scrollbars. 
+# </div>
 
 
-# name tag
 
-# df_distinct_state <- distinct(df1_test, State)
-# df1_test_1 <- df1 %>% filter(State == "CALIFORNIA")
-# 
-# df1_test_1_stat <- df1_test_1 %>% group_by(type, Pesticide_old) %>% summarise(count = n())
-# 
-# df1_test_1_stat[order(df1_test_1_stat$count, decreasing = T),]
-#   
-# p1 <- df1_test_1_stat %>% ggplot() + geom_bar(mapping = aes(x = as.factor(type), y = count, fill = Pesticide_old), 
-#                                       stat = "identity", alpha = 0.5) +
-#   theme_bw() +
-#   labs(x = "State", y = "Pesticide_name")
-# 
-# p2 <- df1_test_1_stat %>% ggplot() + geom_
-# 
-# plot_pie <- function(year, dname1){
-#   
-# }
